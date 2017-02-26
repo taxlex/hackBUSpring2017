61166793e7b6680cd1a613162e7b62808d78f429
Alexander Benedict <abenedi1@binghamton.edu>
2017-02-26 05:19:46 -0500

    flask

---
+++ springboot/hack/hack2017/src/main/java/com/aplalb/Hack2017Application.java
@@ -0,0 +1,29 @@
+package com.aplalb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Hack2017Application {
+	@ResponseBody
+	@RequestMapping("/a")
+	String entry(){
+		return "My Spring Boot App rere";
+	}
+	@ResponseBody
+	@RequestMapping("/h")
+	String test(){
+		return "Added a string";
+	}
+	@RequestMapping("/")
+	String index() {
+	    return"index.html";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Hack2017Application.class, args);
+	}
+}
